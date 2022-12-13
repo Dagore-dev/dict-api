ce14583514519899ec6d3df9a9f513b8e5b9dbef
Dagore <david.gomez.redondo.dev@gmail.com>
2022-12-13 19:46:37 +0100

    REFACTOR validations from service to controller

--- src/controllers/spanishWordsController.js
+++ src/controllers/spanishWordsController.js
@@ -13,7 +13,8 @@ export {
 async function getRandomController (request, response) {
   const { query } = request
   const { minLength, maxLength } = query
-  const obj = await getRandomService(minLength, maxLength)
+  const [min, max] = getFilteredLimits(minLength, maxLength)
+  const obj = await getRandomService(min, max)
 
   if (obj.randomWord != null) {
     response.send(obj)
@@ -21,4 +22,16 @@ async function getRandomController (request, response) {
     response.status(404)
     response.send({ message: 'Not found' })
   }
+}
+
+/**
+ * @param {string} minLength
+ * @param {string} maxLength
+ * @returns {number[]}
+ */
+function getFilteredLimits (minLength, maxLength) {
+  const min = minLength && minLength > 0 ? Number(minLength) : 1
+  const max = maxLength && maxLength > 0 ? Number(maxLength) : Infinity
+
+  return [min, max]
 }
--- src/services/spanishWordsService.js
+++ src/services/spanishWordsService.js
@@ -13,9 +13,7 @@ async function getRandomService (minLength, maxLength) {
   const letters = 'abcdefghijklmnñopqrstuvwxyz'
   const randomLetter = letters[Math.floor(Math.random() * letters.length)]
   const obj = await fs.readJSON(`assets/${randomLetter}.json`)
-  const min = minLength && minLength > 0 ? Number(minLength) : 1
-  const max = maxLength && maxLength > 0 ? Number(maxLength) : Infinity
-  const words = obj[randomLetter].filter(word => word.length >= min && word.length <= max)
+  const words = obj[randomLetter].filter(word => word.length >= minLength && word.length <= maxLength)
   const randomWord = words[Math.floor(Math.random() * words.length)]
 
   return { randomWord }
